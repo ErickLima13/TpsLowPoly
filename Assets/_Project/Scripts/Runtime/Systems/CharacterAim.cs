3a54ecdb8be7bd01924fd0fc449e2e5d57a66f80
Erick Araujo <erickequipe7@gmail.com>
2024-08-06 11:03:02 -0300

    start animation rigging

--- Assets/_Project/Scripts/Runtime/Systems/CharacterAim.cs
+++ Assets/_Project/Scripts/Runtime/Systems/CharacterAim.cs
@@ -1,11 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class CharacterAim : MonoBehaviour
 {
     public float turnSpeed = 15f;
     private Camera mainCamera;
+
+    [Header("Mira")]
+    public float aimDuration = 0.2f;
+    public Rig rigLayerWepPos2;
+    private bool isAim;
 
     private void Start()
     {
@@ -14,12 +18,35 @@ public class CharacterAim : MonoBehaviour
         Cursor.visible = false;
     }
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            isAim = true;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            isAim = false;
+        }
+
+        if (isAim)
+        {
+            rigLayerWepPos2.weight += 
+                Time.deltaTime / aimDuration;
+        }
+        else
+        {
+            rigLayerWepPos2.weight -=
+                Time.deltaTime / aimDuration;
+        }
+    }
+
     private void FixedUpdate()
     {
         float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Slerp
             (transform.rotation,
-            Quaternion.Euler(0, yawCamera, 0), 
+            Quaternion.Euler(0, yawCamera, 0),
             turnSpeed * Time.fixedDeltaTime);
 
     }
