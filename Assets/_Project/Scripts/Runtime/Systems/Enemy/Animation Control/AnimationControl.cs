c283f518394d91448acf73170617b069878083fc
Erick Araujo <erickequipe7@gmail.com>
2024-08-22 12:01:39 -0300

    Start AI Enemy

--- Assets/_Project/Scripts/Runtime/Systems/Enemy/Animation Control/AnimationControl.cs
+++ Assets/_Project/Scripts/Runtime/Systems/Enemy/Animation Control/AnimationControl.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class AnimationControl : MonoBehaviour
 {
     private Animator animator;
+    public int idRandom;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        idRandom = Random.Range(0, 2);
+        animator.SetInteger("idW", idRandom);
     }
 
     [ContextMenu("Pistol")]
